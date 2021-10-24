b81ed099c29fcba367593d113a87bf8a2628b9e7
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-10-24 14:29:17 -0400

    add the js version of the html to make life easy

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const { table } = require("console");
+
 console.log("yo")
 document.addEventListener("click", function(event){
     console.log("click");
@@ -11,4 +13,34 @@ document.addEventListener("click", function(event){
         test.style.backgroundColor = "red";
         console.log("blue");
     }
-})
+});
+
+// table = document.getElementById("tab");
+
+// row = document.createElement("tr");
+// row.id = "0";
+// table.appendChild(row);
+
+// row = document.createElement("tr");
+// row.id = "0";
+// table.appendChild(row);
+
+for(let i = 0; i < 2; i++){
+    let table = document.createElement("table");
+    table.id = "play" + i;
+    document.body.appendChild(table);
+    for(let r = 0; r < 3; r++){
+        let row = document.createElement("tr");
+        table.appendChild(row);
+        for(let d = 0; d < 3; d++){
+            let data = document.createElement("td");
+            data.id = i + ''  + r + '' + d;
+            data.className = "el";
+            data.appendChild(document.createTextNode("row"));
+            row.appendChild(data);
+        }
+    }
+}
+
+// console.log(row.id, table);
+// td = get.createElement
