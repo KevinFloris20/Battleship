b7e5327e18eeb92ca786372eb4204bc3384cb99d
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-10-25 13:01:09 -0400

    added comments and player id's

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// const { table } = require("console");
+let player1Arr = new Array();
+let player2Arr = new Array();
 
-console.log("yo")
+//make board responsive
 document.addEventListener("click", function(event){
     console.log("click");
     console.log(event.target.id);
@@ -15,6 +16,42 @@ document.addEventListener("click", function(event){
     }
 });
 
+//make the board
+for(let i = 0; i < 2; i++){
+    let table = document.createElement("table");
+    table.id = "play" + i;
+    document.body.appendChild(table);
+    for(let r = 0; r < 3; r++){
+        let row = document.createElement("tr");
+        table.appendChild(row);
+        for(let d = 0; d < 3; d++){
+            //make the el
+            let data = document.createElement("td");
+            data.id = i + ''  + r + '' + d;
+            data.className = "el";
+
+            //append to html
+            data.appendChild(document.createTextNode("row"));
+            row.appendChild(data);
+
+            //add new ids to arr
+            if(i == 0){
+                player1Arr.push(data.id);
+            }
+            else if(i == 1){
+                player2Arr.push(data.id);
+            }
+        }
+    }
+}
+
+
+//some rando trashh:
+// const { table } = require("console");
+
+// console.log(row.id, table);
+// td = get.createElement
+
 // table = document.getElementById("tab");
 
 // row = document.createElement("tr");
@@ -25,22 +62,4 @@ document.addEventListener("click", function(event){
 // row.id = "0";
 // table.appendChild(row);
 
-for(let i = 0; i < 2; i++){
-    let table = document.createElement("table");
-    table.id = "play" + i;
-    document.body.appendChild(table);
-    for(let r = 0; r < 3; r++){
-        let row = document.createElement("tr");
-        table.appendChild(row);
-        for(let d = 0; d < 3; d++){
-            let data = document.createElement("td");
-            data.id = i + ''  + r + '' + d;
-            data.className = "el";
-            data.appendChild(document.createTextNode("row"));
-            row.appendChild(data);
-        }
-    }
-}
 
-// console.log(row.id, table);
-// td = get.createElement
