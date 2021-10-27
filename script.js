b12715f28e065900b982f3a1a21e006562c2a4bb
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-10-27 17:18:56 -0400

    gave player1 a random selection and removed tests but the console.logs can stay :3

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ let player2Arr = new Array();
 
 //make board responsive
 document.addEventListener("click", function(event){
-    console.log("click");
-    console.log(event.target.id);
-    test = document.getElementById(event.target.id);
-    console.log(test);
-    if(test.style.backgroundColor ==  "red" ){
-        test.style.backgroundColor = "white";
+    console.log("click ;)");
+
+    function colorChange(){
+        console.log(event.target.id);
+        targetEl = document.getElementById(event.target.id);
+        console.log(targetEl);
+        if(targetEl.style.backgroundColor ==  "red" ){
+            targetEl.style.backgroundColor = "white";
+        }
+        else{
+            targetEl.style.backgroundColor = "red";
+            console.log("blue");
+        }
+    }
+    if(player1Arr.includes(event.target.id) || player2Arr.includes(event.target.id)){
+        colorChange();
     }
     else{
-        test.style.backgroundColor = "red";
-        console.log("blue");
+        console.log("click a real button u dumb butt");
     }
 });
 
@@ -21,10 +30,10 @@ for(let i = 0; i < 2; i++){
     let table = document.createElement("table");
     table.id = "play" + i;
     document.body.appendChild(table);
-    for(let r = 0; r < 3; r++){
+    for(let r = 0; r < 8; r++){
         let row = document.createElement("tr");
         table.appendChild(row);
-        for(let d = 0; d < 3; d++){
+        for(let d = 0; d < 8; d++){
             //make the el
             let data = document.createElement("td");
             data.id = i + ''  + r + '' + d;
@@ -45,8 +54,29 @@ for(let i = 0; i < 2; i++){
     }
 }
 
+//-------make game rules-------
 
-//some rando trashh:
+//  make a random selection for computer
+let randArr1 = new Array();
+for(let x = 0; x < 8; x++){
+    let randID = player1Arr[Math.floor(Math.random() * player1Arr.length)]
+    if(randArr1.includes(randID)){
+        console.log(randID);
+        x--;
+    }
+    else{
+        randArr1[x] = randID;
+        document.getElementById(randArr1[x]).style.backgroundColor = "red";
+        console.log(randID)
+    }
+}
+
+
+
+
+
+//some random trash
+//  console.log(player1Arr[Math.floor(Math.random() * player1Arr.length)])    <-- litterly random trash ;)
 // const { table } = require("console");
 
 // console.log(row.id, table);
